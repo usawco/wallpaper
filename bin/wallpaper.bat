77be0810b2edc297d14638c127cb40211bfcd1aa
Warren Cooper <whc.ashburn@gmail.com>
2018-03-17 07:20:37 -0400

    fix path reference to wp.ps1

--- bin/wallpaper.bat
+++ bin/wallpaper.bat
@@ -1,1 +1,4 @@
-powershell.exe -NoProfile -ExecutionPolicy bypass -Command ./wp.ps1
+@echo off
+setlocal enableextensions
+set _DIR=%~dp0
+powershell.exe -NoProfile -ExecutionPolicy bypass -Command %_DIR%wp.ps1
